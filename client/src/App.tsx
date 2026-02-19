434fa9a8e4c8831aab4954038b619007493b68cc
Nenad-777 <hnmaricic@gmail.com>
2026-02-19 08:02:06 +0100

    Refactor routes in App.tsx for clarity and organization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,52 +2,38 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
+
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 import Home from "./pages/Home";
-import GeopolitikaArticle from "./pages/GeopolitikaArticle";
-import GeopolitikaIran from "./pages/GeopolitikaIran";
 import GeopolitikaIndex from "./pages/GeopolitikaIndex";
+import GeopolitikaIran from "./pages/GeopolitikaIran";
+import GeopolitikaArticle from "./pages/GeopolitikaArticle";
+
 import ObavestajniArticle from "./pages/ObavestajniArticle";
 import SrbijaPage from "./pages/SrbijaPage";
 
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
+      {/* Home */}
+      <Route path="/" component={Home} />
 
       {/* Geopolitika */}
-      <Route path={"/geopolitika"} component={GeopolitikaIndex} />
-      <Route path={"/geopolitika/"} component={GeopolitikaIndex} />
-
-      <Route path={"/geopolitika/iran"} component={GeopolitikaIran} />
-      <Route path={"/geopolitika/iran/"} component={GeopolitikaIran} />
-
+      <Route path="/geopolitika/iran" component={GeopolitikaIran} />
       <Route
-        path={"/geopolitika/nova-bezbednosna-arhitektura"}
+        path="/geopolitika/nova-bezbednosna-arhitektura"
         component={GeopolitikaArticle}
       />
-      <Route
-        path={"/geopolitika/nova-bezbednosna-arhitektura/"}
-        component={GeopolitikaArticle}
-      />
+      <Route path="/geopolitika" component={GeopolitikaIndex} />
 
-      {/* Ostalo */}
-      <Route
-        path={"/obavestajni-izvori/rat-senki"}
-        component={ObavestajniArticle}
-      />
-      <Route
-        path={"/obavestajni-izvori/rat-senki/"}
-        component={ObavestajniArticle}
-      />
+      {/* Ostale rubrike */}
+      <Route path="/obavestajni-izvori/rat-senki" component={ObavestajniArticle} />
+      <Route path="/srbija" component={SrbijaPage} />
 
-      <Route path={"/srbija"} component={SrbijaPage} />
-      <Route path={"/srbija/"} component={SrbijaPage} />
-
-      {/* Fallback */}
-      <Route path={"/404"} component={NotFound} />
+      {/* 404 */}
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
