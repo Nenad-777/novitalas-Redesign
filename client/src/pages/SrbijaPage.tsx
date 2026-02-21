ab424cc2c35d03b12a15aa3cf0db1d5b2b64fe49
Nenad-777 <hnmaricic@gmail.com>
2026-02-21 20:26:50 +0100

    Fix missing span closing tag

--- client/src/pages/SrbijaPage.tsx
+++ client/src/pages/SrbijaPage.tsx
@@ -166,7 +166,7 @@ export default function SrbijaPage() {
                     Srbija
                   </span>
                   <span>&nbsp;&bull;&nbsp;</span>
-                  <span>Analiza nedelje 
+                  <span>Analiza nedelje</span>
                 </div>
 
                 <h2
