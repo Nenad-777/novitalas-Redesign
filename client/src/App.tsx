67c69d94f61b3b9b1cc5ad4ddf7ad58b86af064c
Nenad-777 <hnmaricic@gmail.com>
2026-02-25 22:22:04 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import GeopolitikaArticle from "./pages/GeopolitikaArticle";
 
 // ✅ VAŽNO: import mora da pogodi TAČNO ime fajla u /pages
 import UkrajinaCetiriGodine from "./pages/ukrajina-cetiri-godine-rata";
+import IranProtesti2026 from "./pages/iran-protesti-2026";
 
 import ObavestajniArticle from "./pages/ObavestajniArticle";
 import SrbijaPage from "./pages/SrbijaPage";
@@ -48,6 +49,16 @@ function Router() {
         component={UkrajinaCetiriGodine}
       />
 
+      {/* ✅ NOVA VEST: Iran – protesti */}
+      <Route
+        path="/geopolitika/iran-protesti-2026"
+        component={IranProtesti2026}
+      />
+      <Route
+        path="/geopolitika/iran-protesti-2026/"
+        component={IranProtesti2026}
+      />
+
       <Route path="/geopolitika" component={GeopolitikaIndex} />
       <Route path="/geopolitika/" component={GeopolitikaIndex} />
 
@@ -55,17 +66,32 @@ function Router() {
       <Route path="/obavestajni-izvori" component={ObavestajniArticle} />
       <Route path="/obavestajni-izvori/" component={ObavestajniArticle} />
 
-      <Route path="/obavestajni-izvori/rat-senki" component={ObavestajniArticle} />
-      <Route path="/obavestajni-izvori/rat-senki/" component={ObavestajniArticle} />
+      <Route
+        path="/obavestajni-izvori/rat-senki"
+        component={ObavestajniArticle}
+      />
+      <Route
+        path="/obavestajni-izvori/rat-senki/"
+        component={ObavestajniArticle}
+      />
 
       {/* Srbija */}
       <Route path="/srbija" component={SrbijaPage} />
       <Route path="/srbija/" component={SrbijaPage} />
-      <Route path="/srbija/mars-za-pravosudje" component={SrbijaMarsZaPravosudje} />
-      <Route path="/srbija/mars-za-pravosudje/" component={SrbijaMarsZaPravosudje} />
+      <Route
+        path="/srbija/mars-za-pravosudje"
+        component={SrbijaMarsZaPravosudje}
+      />
+      <Route
+        path="/srbija/mars-za-pravosudje/"
+        component={SrbijaMarsZaPravosudje}
+      />
 
       <Route path="/srbija/podeljeno-drustvo" component={SrbijaPolarizacija} />
-      <Route path="/srbija/podeljeno-drustvo/" component={SrbijaPolarizacija} />
+      <Route
+        path="/srbija/podeljeno-drustvo/"
+        component={SrbijaPolarizacija}
+      />
 
       {/* 404 */}
       <Route path="/404" component={NotFound} />
