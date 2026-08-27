c7c82421acbb8a5d5e1b83d88748bf88baabbc3b
Nenad-777 <hnmaricic@gmail.com>
2026-08-27 19:45:24 +0200

    Normalize Ratcliffe headline on intelligence index

--- client/src/pages/ObavestajniIndex.tsx
+++ client/src/pages/ObavestajniIndex.tsx
@@ -13,7 +13,7 @@ const ARTICLES = [
     href: "/obavestajni-izvori/retklifova-misija-u-moskvi-dobija-nove-obrise",
     img: "/news/ratcliffe-moscow-red-line.jpg",
     alt: "Grafička ilustracija tajne američke misije u Moskvi: zvaničnik prelazi crvenu liniju na aerodromskoj pisti dok se u pozadini nalazi vojni transportni avion.",
-    title: "RETKLIFOVA MISIJA U MOSKVI DOBIJA NOVE OBRISE",
+    title: "Retklifova misija u Moskvi dobija nove obrise",
     description:
       "Dan nakon što je potvrđena nenajavljena poseta direktora CIA Džona Retklifa Moskvi, pojavili su se prvi konkretniji podaci o njegovim razgovorima. Šef ruske Spoljne obaveštajne službe Sergej Nariškin potvrdio je da se lično sastao sa Retklifom, dok američki mediji navode da je jedna od tema bilo upozorenje Moskvi da ne pokušava eskalaciju prema članicama NATO-a. Donald Tramp takvu interpretaciju, međutim, javno odbacuje.",
     date: "27. avgust 2026.",
