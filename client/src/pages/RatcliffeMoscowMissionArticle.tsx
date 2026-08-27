bafc42976004242a03fb341fb9dca27647a7e524
Nenad-777 <hnmaricic@gmail.com>
2026-08-27 19:44:39 +0200

    Normalize Ratcliffe article title casing

--- client/src/pages/RatcliffeMoscowMissionArticle.tsx
+++ client/src/pages/RatcliffeMoscowMissionArticle.tsx
@@ -42,7 +42,7 @@ export default function RatcliffeMoscowMissionArticle() {
     <ArticleTemplate
       path={PATH}
       sectionLabel="Obaveštajni izvori"
-      title="RETKLIFOVA MISIJA U MOSKVI DOBIJA NOVE OBRISE"
+      title="Retklifova misija u Moskvi dobija nove obrise"
       dateLabel="27. avgust 2026."
       deck="Dan nakon što je potvrđena nenajavljena poseta direktora CIA Džona Retklifa Moskvi, pojavili su se prvi konkretniji podaci o njegovim razgovorima. Šef ruske Spoljne obaveštajne službe Sergej Nariškin potvrdio je da se lično sastao sa Retklifom, dok američki mediji navode da je jedna od tema bilo upozorenje Moskvi da ne pokušava eskalaciju prema članicama NATO-a. Donald Tramp takvu interpretaciju, međutim, javno odbacuje."
       imageSrc="/news/ratcliffe-moscow-red-line.jpg"
